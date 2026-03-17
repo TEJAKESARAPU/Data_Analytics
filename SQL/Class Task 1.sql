5bf021cf2e91455f387a6d465ad51373a6deb72c
TEJA KESARAPU <145187633+TEJAKESARAPU@users.noreply.github.com>
2026-03-17 16:44:02 +0530

    Add files via upload

---
+++ SQL/Class Task 1.sql
@@ -0,0 +1,74 @@
+Create table employees (id int, 
+						name varchar(20), 
+                        department varchar(20), 
+                        age int, 
+                        salary decimal(8,2));
+                        
+INSERT INTO employees (id, name, department, age, salary) VALUES
+(1, 'Alice', 'HR', 28, 45000.00),
+(2, 'Bob', 'IT', 35, 70000.00),
+(3, 'Charlie', 'Sales', 30, 52000.00),
+(4, 'David', 'Finance', 45, 80000.00),
+(5, 'Eva', 'Admin', 22, 40000.00),
+(6, 'Frank', 'IT', 38, 65000.00),
+(7, 'Grace', 'HR', 26, 48000.00),
+(8, 'Hannah', 'Sales', 29, 51000.00),
+(9, 'Ian', 'Finance', 50, 82000.00),
+(10, 'Jane', 'IT', 24, 47000.00),
+(11, 'Karl', 'Admin', 31, 39000.00),
+(12, 'Liam', 'Sales', 42, 55000.00),
+(13, 'Mona', 'Finance', 34, 60000.00),
+(14, 'Nina', 'IT', 27, 43000.00),
+(15, 'Oscar', 'HR', 40, 53000.00),
+(16, 'Paul', 'Sales', 23, 41000.00),
+(17, 'Quinn', 'IT', 37, 62000.00),
+(18, 'Rachel', 'Admin', 33, 46000.00),
+(19, 'Steve', 'Finance', 39, 75000.00),
+(20, 'Tina', 'HR', 25, 50000.00);
+
+select * from employees;
+
+-- Q1. Retreive all employees who are older than 35.
+select name, age from employees where age > 35;
+
+-- 2. Find employees with a salary less than or equal to 45000.
+select name, salary from employees where salary >= 45000;
+
+-- 3. List employees whose age is exactly 25.
+select name, age from employees where age = 25;
+
+-- 4. Find employees older than 30 and working in the 'Finance' department.
+select name, department from employees where age > 30 and department = "Finance";
+
+-- 5. Find employees who work in the 'IT' or 'Sales' department.
+select name, department from employees where department = "IT" or department = "Sales";
+
+-- 6. Get all employees whose age is not 40.
+select name, age from employees where not age = 40;
+
+-- 7. Find employees whose salary is between 45000 and 60000.
+select name, salary from employees where salary between 45000 and 60000;
+
+-- 8. Retrieve employees whose age is not between 30 and 45.
+select name, age from employees where not age between 30 and 45;
+
+-- 9. Get all employees whose department is either 'HR', 'Admin', or 'Finance'.
+select name, department from employees where department in ("HR", "Admin", "Finance");
+
+-- 10. List the top 3 highest-paid employees.
+select name, salary from employees order by salary desc limit 3;
+
+-- 11. Get the 5 youngest employees in the company.
+select name, age from employees order by age asc limit 5;
+
+-- 12. Skip the first 5 employees and display the next 5 based on id order.
+select name id, name from employees order by id limit 5 offset 5;
+
+-- 13. Find employees who do not work in the 'Sales' department.
+select name, department from employees where not department = 'Sales';
+
+-- 14. Get employees with salary greater than 60000 or age less than 26.
+select name, age, salary from employees where salary > 60000 or age < 26;
+
+-- 15. Display all employees sorted by salary in descending order
+select name, salary from employees order by salary desc;
---
+++ SQL/train tasks.sql
@@ -0,0 +1,31 @@
+Select * from train;
+
+-- Q1. Find all passengers who were younger than 18.
+select name, age from train where age < 18;
+
+-- Q2. List all passengers who paid a fare greater than 50.
+select name, Fare from train where Fare > 50;
+
+-- Q3. Get passengers who were in 1st or 2nd class.
+select name, Pclass from train where Pclass between 1 and 2;
+
+-- Q4. Retrieve all female passengers who survived.
+select name from train where Sex = 'Female' and Survived = 1;
+
+-- Q5. List passengers who were either children (age < 18) or paid a fare more than 100.
+select name from train where Age < 18 or Fare > 100;
+
+-- Q6. Find all passengers who embarked from 'C'(Cherbourg) and in 3rd class.
+select name from train where Embarked = 'C' and Pclass = 3;
+
+-- Q7. Find passengers who are NOT male (i.e., female or other).
+select name from train where not Sex = 'male'; 
+
+-- Q8. Get passengers aged between 20 and 40 inclusive.
+select name from train where Age between 20 and 40;
+
+-- Q9. Find all passengers who had more than 20 siblings/spouses aboard and paid less than 20 fare.
+select name from train where SibSp > 20 and Fare < 20;
+
+-- Q10. List passengers who survived AND were 1st class AND paid a fare greater than or equal to 100.
+select name from train where Survived = 1 and Pclass = 1 and Fare >= 100;
